708236ca0dda7031da2660c28d9c6756d2e27512
Lomowg <sokol789123@yandex.ru>
2024-07-03 18:01:10 +0300

    Added working bot with basic menu

---
+++ main.py
@@ -0,0 +1,38 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+from handlers import other_handlers, user_handlers
+from config_data.config import Config, load_config
+
+
+logger = logging.getLogger(__name__)
+
+
+async def main():
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format='%(filename)s:%(lineno)d #%(levelname)-8s '
+               '[%(asctime)s] - %(name)s - %(message)s')
+
+    logger.info('Starting bot')
+
+    config: Config = load_config()
+
+    bot = Bot(
+        token=config.tg_bot.token,
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML)
+    )
+    dp = Dispatcher()
+
+    dp.include_router(user_handlers.router)
+    dp.include_router(other_handlers.router)
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+asyncio.run(main())
