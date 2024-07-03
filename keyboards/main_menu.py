eac1da35e42b29400f7a1b45390ce5cb93cde0da
Lomowg <sokol789123@yandex.ru>
2024-07-03 15:53:41 +0300

    Added main menu func

---
+++ keyboards/main_menu.py
@@ -0,0 +1,11 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+
+def create_main_menu() -> InlineKeyboardMarkup:
+    keyboard = InlineKeyboardMarkup(
+        inline_keyboard=[[InlineKeyboardButton(text='🗃️ Хранилище', callback_data='storage')],
+                         [InlineKeyboardButton(text='⚙️ Настройки', callback_data='settings')],
+                         [InlineKeyboardButton(text='⁉️ Помощь', callback_data='help')]]
+    )
+
+    return keyboard
