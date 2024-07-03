d73e71a2506028b0a121813158966c9550ac76f3
Lomowg <sokol789123@yandex.ru>
2024-07-03 18:00:36 +0300

    Added basic handlers

---
+++ handlers/user_handlers.py
@@ -0,0 +1,144 @@
+from aiogram import F, Router
+from aiogram.filters import Command, CommandStart
+from aiogram.types import CallbackQuery, Message
+from aiogram.types import InlineKeyboardButton
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+from lexicon.lexicon_general import LEXICON
+
+from lexicon.lexicon_ru import LEXICON_RU
+from keyboards.settings_menu import create_settings_menu, create_language_menu
+from keyboards.main_menu import create_main_menu
+from keyboards.storage_menu import create_dirs_menu
+
+router = Router()
+
+
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    # if message.from_user.id not in users_db: сделать проверку на нахождение в бд
+    #     pass
+
+    text = LEXICON_RU[message.text]
+
+    await message.answer(
+        text=text,
+        reply_markup=create_main_menu()
+    )
+
+
+@router.callback_query(F.data == 'storage')
+async def process_storage_command(callback: CallbackQuery):
+    text = LEXICON_RU[callback.data]
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=create_dirs_menu()
+    )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data == 'settings')
+async def process_settings_command(callback: CallbackQuery):
+    text = LEXICON_RU[callback.data]
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=create_settings_menu()
+    )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data == 'language')
+async def process_language_command(callback: CallbackQuery):
+    text = LEXICON_RU[callback.data]
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=create_language_menu()
+    )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data.in_(['lan_RU', 'lan_EN']))
+async def process_language_choice_command(callback: CallbackQuery):
+    # Реализовать смену языка
+
+    text = LEXICON_RU[callback.data]
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=create_language_menu()
+    )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data == 'menu')
+async def process_menu_command(callback: CallbackQuery):
+    text = LEXICON_RU[callback.data]
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=create_main_menu()
+    )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data == 'create_dir')
+async def process_create_dir_command(callback: CallbackQuery):
+    text = LEXICON_RU[callback.data]
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=create_dirs_menu()
+    )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data == 'delete_dir')
+async def process_delete_dir_command(callback: CallbackQuery):
+    text = LEXICON_RU[callback.data]
+
+    # Реализовать удаление папок
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=create_dirs_menu()
+    )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data == 'help')
+async def process_help_command(callback: CallbackQuery):
+    text = LEXICON_RU[callback.data]
+
+    kb_builder = InlineKeyboardBuilder()
+    kb_builder.row(InlineKeyboardButton(text=f'{LEXICON['menu']} Меню', callback_data='menu'))
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=kb_builder.as_markup()
+    )
+
+    await callback.answer()
+
+
+@router.callback_query(F.data == 'to_author')
+async def process_create_dir_command(callback: CallbackQuery):
+    text = LEXICON_RU[callback.data]
+
+    kb_builder = InlineKeyboardBuilder()
+    kb_builder.row(InlineKeyboardButton(text='⬅️ Назад', callback_data='settings'))
+
+    await callback.message.edit_text(
+        text=text,
+        reply_markup=kb_builder.as_markup()
+    )
+
+    await callback.answer()
