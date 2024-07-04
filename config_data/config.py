364c6f7697b98ed782f2677fac9568018932e242
Lomowg <sokol789123@yandex.ru>
2024-07-04 17:44:35 +0300

    Added some classes for database

--- config_data/config.py
+++ config_data/config.py
@@ -4,6 +4,27 @@ from environs import Env
 
 
 @dataclass
+class UserDB:
+    user: str
+    password: str
+
+
+@dataclass
+class DB:
+    host: str
+    port: int
+    db_name: str
+
+
+@dataclass
+class ConnectionsPool:
+    db: DB
+    user: UserDB
+    min_size: int
+    max_size: int
+
+
+@dataclass
 class TgBot:
     token: str
 
@@ -11,9 +32,18 @@ class TgBot:
 @dataclass
 class Config:
     tg_bot: TgBot
+    con_pool: ConnectionsPool
 
 
 def load_config(path: str | None = None) -> Config:
     env = Env()
     env.read_env(path)
-    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')))
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN')),
+                  con_pool=ConnectionsPool(db=DB(host=env('HOST'),
+                                                 port=int(env('PORT')),
+                                                 db_name=env('DATABASE')),
+                                           user=UserDB(user=env('USER'),
+                                                       password=env('PASSWORD')),
+                                           min_size=int(env('POOL_MIN_SIZE')),
+                                           max_size=int(env('POOL_MAX_SIZE'))))
+
