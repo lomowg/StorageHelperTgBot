53537ffd939bbb3c9e70d0807a7dc2affba519b6
Lomowg <sokol789123@yandex.ru>
2024-07-04 19:29:59 +0300

    Added midlewares for database

---
+++ middlewares/db_middleware.py
@@ -0,0 +1,21 @@
+from typing import Callable, Dict, Any, Awaitable
+from aiogram import BaseMiddleware
+from aiogram.types import TelegramObject
+from asyncpg.pool import Pool
+from database.database import DataBaseClass
+
+
+class DataBaseMiddleware(BaseMiddleware):
+    def __init__(self, pool: Pool):
+        super().__init__()
+        self.pool = pool
+
+    async def __call__(
+            self,
+            handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
+            event: TelegramObject,
+            data: Dict[str, Any]
+    ) -> Any:
+        async with self.pool.acquire() as connect:
+            data['database'] = DataBaseClass(connect)
+            return await handler(event, data)
