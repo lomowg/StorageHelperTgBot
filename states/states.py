4990c9dba5e64e6d9b6a53199bf383efa8ed9a19
Lomowg <sokol789123@yandex.ru>
2024-07-05 18:02:40 +0300

    Removed in_menu state

--- states/states.py
+++ states/states.py
@@ -2,7 +2,5 @@ from aiogram.filters.state import StatesGroup, State
 
 
 class FSMStorageManipulating(StatesGroup):
-    in_menu = State()
     in_folder = State()
     write_folder_name = State()
-    
