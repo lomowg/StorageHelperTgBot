19215bddf3df69c08b6a090ae6c4a2713edad891
Lomowg <sokol789123@yandex.ru>
2024-07-05 19:00:53 +0300

    Added remove keyboard

--- keyboards/storage_menu.py
+++ keyboards/storage_menu.py
@@ -30,3 +30,21 @@ def create_dirs_menu(*args: int) -> InlineKeyboardMarkup:
 
     return kb_builder.as_markup()
 
+
+def create_edit_keyboard(*args: int) -> InlineKeyboardMarkup:
+
+    kb_builder = InlineKeyboardBuilder()
+
+    for button in sorted(args):
+        kb_builder.row(InlineKeyboardButton(
+            text=f'{LEXICON["del"]} {str(button).replace('_fldBtn', '')}',
+            callback_data=f'{str(button).replace('_fldBtn', '')}del'
+        ))
+
+    kb_builder.row(
+        InlineKeyboardButton(
+            text='⬅️ Отменить',
+            callback_data='storage'
+        )
+    )
+    return kb_builder.as_markup()
