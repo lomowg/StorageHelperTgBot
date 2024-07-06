d70fec454261814b32a2b3a952c448a3a451ffb3
Lomowg <sokol789123@yandex.ru>
2024-07-06 15:11:35 +0300

    Added menu button functon

--- keyboards/main_menu.py
+++ keyboards/main_menu.py
@@ -1,4 +1,6 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram import Bot
+from aiogram.types import BotCommand
 
 
 def create_main_menu() -> InlineKeyboardMarkup:
@@ -11,3 +13,10 @@ def create_main_menu() -> InlineKeyboardMarkup:
     return keyboard
 
 
+async def set_main_menu(bot: Bot):
+    main_menu_commands = [
+        BotCommand(command='/menu',
+                   description='Вызов главного меню'),
+    ]
+
+    await bot.set_my_commands(main_menu_commands)
