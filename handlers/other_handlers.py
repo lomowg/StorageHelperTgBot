5a913764ee011d765d20f50cf7e6c3b337e24e83
Lomowg <sokol789123@yandex.ru>
2024-07-07 11:41:15 +0300

    Added any message answer

--- handlers/other_handlers.py
+++ handlers/other_handlers.py
@@ -4,6 +4,7 @@ from keyboards.main_menu import create_main_menu
 
 router = Router()
 
+
 @router.message()
-async def send_echo(message: Message):
-    await message.answer(f'Это эхо! {message.text}', reply_markup=create_main_menu())
+async def any_message(message: Message):
+    await message.answer(f'Я работаю только через меню:', reply_markup=create_main_menu())
