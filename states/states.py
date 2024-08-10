32ff94ccd3e6047dcf7725d8f17206c08a7a5032
Lomowg <sokol789123@yandex.ru>
2024-08-10 17:35:33 +0300

    Added new state for renaming folder

--- states/states.py
+++ states/states.py
@@ -4,3 +4,4 @@ from aiogram.filters.state import StatesGroup, State
 class FSMStorageManipulating(StatesGroup):
     in_folder = State()
     write_folder_name = State()
+    rename_folder = State()
