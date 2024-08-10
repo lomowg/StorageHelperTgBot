10360b06b06aa426ca7e56df8f2ec879d12f89b4
Lomowg <sokol789123@yandex.ru>
2024-08-10 18:51:26 +0300

    Added rename keyboard

--- keyboards/storage_menu.py
+++ keyboards/storage_menu.py
@@ -26,6 +26,8 @@ def create_dirs_menu(*args: int) -> InlineKeyboardMarkup:
         width=2
     )
 
+    kb_builder.row(InlineKeyboardButton(text='✏️ Переименовать папку', callback_data='rename_dir'))
+
     kb_builder.row(InlineKeyboardButton(text=f'{LEXICON['menu']} Меню', callback_data='menu'))
 
     return kb_builder.as_markup()
@@ -39,6 +41,25 @@ def create_edit_keyboard(*args: int) -> InlineKeyboardMarkup:
         kb_builder.row(InlineKeyboardButton(
             text=f'{LEXICON["del"]} {str(button).replace('_fldBtn', '')}',
             callback_data=f'{str(button).replace('_fldBtn', '')}del'
+        ))
+
+    kb_builder.row(
+        InlineKeyboardButton(
+            text='⬅️ Отменить',
+            callback_data='storage'
+        )
+    )
+    return kb_builder.as_markup()
+
+
+def create_rename_keyboard(*args: int) -> InlineKeyboardMarkup:
+
+    kb_builder = InlineKeyboardBuilder()
+
+    for button in sorted(args):
+        kb_builder.row(InlineKeyboardButton(
+            text=f'✏️ {str(button).replace('_fldBtn', '')}',
+            callback_data=f'{str(button).replace('_fldBtn', '')}rename'
         ))
 
     kb_builder.row(
